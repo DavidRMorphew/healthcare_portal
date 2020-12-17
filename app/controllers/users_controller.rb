a5afc07217da7cbc6a75e202bcfdf3c9b4427120
DavidRMorphew <morphewd@umich.edu>
2020-12-16 22:12:28 -0500

    uniqueness validation added with modified error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
     end
 
     post "/signup" do
-        user = User.new(params[:users])
+        user = User.new(params[:user])
         if user.save
             session[:user_id] = user.id
             redirect "/patients"
         else
+            binding.pry
             @errors = user.errors.full_messages
             erb :"users/new"
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :patients
     validates :username, presence: true
-    validates :username, uniqueness: true
+    validates :username, uniqueness: { message: "has already been taken. Please choose a unique username"}
 end
