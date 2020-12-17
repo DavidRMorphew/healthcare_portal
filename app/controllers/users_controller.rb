131c4c5cf47f169f279a41c2bb406ceca7fa06e2
DavidRMorphew <morphewd@umich.edu>
2020-12-16 21:23:58 -0500

    building users_controller post /signup & uniqueness validation for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
     get "/signup" do
         erb :"users/new"
     end
+
+    post "/signup" do
+        #  check uniqueness of password before creating new user
+        binding.pry
+    end
 end
