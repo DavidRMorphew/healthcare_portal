f005e92182fab22a2ea0529c00d126d0258af973
DavidRMorphew <morphewd@umich.edu>
2020-12-17 16:05:51 -0500

    SessionsController added with #login method, mounted, & sessions/login erb file created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,6 @@ class UsersController < ApplicationController
         end
     end
 
+  
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     validates :username, presence: true
-    validates :username, uniqueness: { message: "has already been taken. Please choose a unique username"} # Check up on how these macros work
+    validates :username, uniqueness: { message: "has already been taken. Please choose a unique username" } # Check up on how these macros work
     has_secure_password
     has_many :patients
 end
