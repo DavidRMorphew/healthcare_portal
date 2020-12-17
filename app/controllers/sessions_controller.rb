e2a1d8e4d87e155c19b9a8239610c9ad4954c03f
DavidRMorphew <morphewd@umich.edu>
2020-12-17 17:05:09 -0500

    post /login route with authentication built & error message for sessions/login created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
     get "/login" do
         erb :"sessions/login"
     end
+
+    post "/login" do
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect "/patients"
+        else
+            # binding.pry
+            @error = "The Username or Password that was entered is incorrect."
+            erb :"sessions/login"
+        end
+    end
 end
