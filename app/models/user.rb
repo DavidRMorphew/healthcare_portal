86ae9f3272463b853a8ef7c9dddf71e958bdd52a
DavidRMorphew <morphewd@umich.edu>
2020-12-21 15:29:23 -0500

    comments removed from user model and layout erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     validates :username, presence: true
-    validates :username, uniqueness: { case_sensitive: false, message: "has already been taken. Please choose a unique username" } # Check up on how these macros work
+    validates :username, uniqueness: { case_sensitive: false, message: "has already been taken. Please choose a unique username" }
     has_secure_password
     has_many :patients
 end
